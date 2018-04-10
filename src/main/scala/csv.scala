b5801729fc429967af6e3bc53da1d5d19195fe2d
Stanislav <kapinuss@rambler.ru>
2018-04-10 16:25:51 +0300

    dates

--- src/main/scala/csv.scala
+++ src/main/scala/csv.scala
@@ -15,8 +15,10 @@ object Login {
 case class PluralLogin(ip: String, start: String, stop: String, users: String)//users = ip1:time1,ip2:time2
 object PluralLogin {
   val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-  def apply(nick: String, ip: String, dateTime: String): Login = new Login(nick, ip, performDate(dateTime))
-  def performDate(dateTime: String): LocalDateTime = LocalDateTime.parse(dateTime, formatter)
+  def apply(ip: String, start: String, stop: String, users: String): PluralLogin = new PluralLogin(ip, start, stop, users)
+  def apply(logins: List[Login]): PluralLogin = new PluralLogin(logins.head.ip, logins.head.dateTime.toString, logins.last.dateTime.toString, formUsers(logins))
+  def performDate(dateTime: LocalDateTime) : String = dateTime.format(formatter)
+  def formUsers(similarLogins: List[Login]): String = {"mock"}
 }
 
 object csv {
