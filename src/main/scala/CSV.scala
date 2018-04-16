b17739dc1f31ad6a1d16ad0ae160d558eb460439
Stanislav <kapinuss@rambler.ru>
2018-04-16 10:54:19 +0300

    exception handling

--- src/main/scala/CSV.scala
+++ src/main/scala/CSV.scala
@@ -1,8 +1,10 @@
-import java.io.File
+import java.io.{File, FileNotFoundException}
 import java.time.LocalDateTime
-import akka.actor.{ActorRef, ActorSystem, Props}
+
+import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.github.tototoshi.csv.{CSVReader, CSVWriter}
+
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -14,15 +16,25 @@ object CSV {
 
   def main(args: Array[String]): Unit = {
 
-    for (rawLogins: List[List[String]] <- readCsv("logins0.csv")) yield {
+    for (rawLogins: List[List[String]] <- readCsv("logins0.csv") recoverWith {
+      case t: FileNotFoundException => {
+        system.log.info("Файл не найден: " + t)
+        Future.failed(new Exception("Файл не найден", t))
+      }
+      case t: Throwable => {
+        system.log.info("Что-то пошло не так: " + t)
+        Future.failed(new Exception("Что-то пошло не так.", t))
+      }
+    }) yield {
       system.log.info(s"Приложение запущено, период 1 час, прочитано ${rawLogins.size} строк из csv файла.")
-      val result: List[List[String]] = makeResult(rawLogins, 1)
-      writeScv("result.csv", result)
-      system.log.info(s"Приложение закончило работу, записано ${result.size} строк в csv файл.")
+      for (result: List[List[String]] <- makeResult(rawLogins, 1)) {
+        writeScv("result.csv", result)
+        system.log.info(s"Приложение закончило работу, записано ${result.size} строк в csv файл.")
+      }
     }
   }
 
-  def makeResult(rawLogins: List[List[String]], hours: Int): List[List[String]] = {
+  def makeResult(rawLogins: List[List[String]], hours: Int): Future[List[List[String]]] = Future {
     val logins: List[LoginWithLong] = rawLogins.map(login => LoginWithLong(login.head, login(1), login.last))
     val reducedLogins: Map[String, List[LoginWithLong]] = logins.groupBy(_.ip).filter(each => each._2.size > 1)
       .map(each => (each._1, each._2.sortBy(_.dateTime))).filter(x => LoginWithLong.isOften(x._2, hours))
